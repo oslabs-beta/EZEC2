fc392a9f400c28427e4926a6fbf8f25b95a8cbb3
James Appleton <jappleton101@gmail.com>
2024-10-24 12:19:48 -0400

    Fixed typo

--- src/server/controllers/cloudwatchController.js
+++ src/server/controllers/cloudwatchController.js
@@ -27,7 +27,7 @@ cloudwatchController.getUsageData = async (req, res, next) => {
               MetricName: 'CPUUtilization',
               Dimensions: [
                 {
-                  Name: 'InstanceId', // requiredls
+                  Name: 'InstanceId', // required
                   Value: instanceId, // required
                 },
               ],
